ca867da250b3d9fe05f208415a6219702f00c0b5
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2019-12-04 09:02:29 -0500

    Convert from Electron to webpage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import Layout from './components/Layout';
 import store from './redux/store';
 import Main from './views/Main';
+import './styles/style.css';
 
 export default function App() {
   return (
--- src/views/Main/index.jsx
+++ src/views/Main/index.jsx
@@ -5,9 +5,9 @@ import LabelSection from './sections/LabelSection';
 
 export default function Main() {
   return (
-    <React.Fragment>
+    <>
       <FormSection className="flex-grow-0 flex-shrink-0" />
       <LabelSection className="flex-grow-1 flex-shrink-1" />
-    </React.Fragment>
+    </>
   );
 }
